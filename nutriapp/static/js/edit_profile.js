b1d322a5235dfe2c7b7f6bc455f139bdb8f3a663
Abong-123 <abrarwirap@gmail.com>
2026-03-28 09:08:34 +0700

    raport

--- nutriapp/static/js/edit_profile.js
+++
@@ -1,9 +0,0 @@
-function previewAvatar(event) {
-  const file = event.target.files[0];
-  if (!file) return;
-  const reader = new FileReader();
-  reader.onload = (e) => {
-    document.getElementById('avatar-preview').src = e.target.result;
-  };
-  reader.readAsDataURL(file);
-}
